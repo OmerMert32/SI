d283ecf86412d19f5fcf7314844f464cacddc521
Ömer Gökhan Mert <omer0204@stud.kea.dk>
2024-02-14 23:16:13 +0100

    assignment 2

---
+++ 00_Assignments/app.js
@@ -0,0 +1,66 @@
+import express from 'express';
+import FileParser from 'file:///C:/Users/omerm/SI/02_Data_Files/reader_parser.js'; 
+
+
+
+
+class DataAPI {
+  constructor() {
+    this.csvData = null;
+    this.jsonData = null;
+    this.txtData = null;
+    this.xmlData = null;
+    this.yamlData = null;
+    this.app = express();
+    this.initRoutes();
+  }
+
+  async loadData() {
+    try {
+      this.csvData = await FileParser.parseCSV('C:/Users/omerm/SI/02_Data_Files/me.csv');
+      this.jsonData = await FileParser.parseJSON('C:/Users/omerm/SI/02_Data_Files/me.json');
+      this.txtData = await FileParser.parseTXT('C:/Users/omerm/SI/02_Data_Files/me.txt');
+      this.xmlData = await FileParser.parseXML('C:/Users/omerm/SI/02_Data_Files/me.xml');
+      this.yamlData = await FileParser.parseYAML('C:/Users/omerm/SI/02_Data_Files/me.yaml');
+    } catch (error) {
+      console.error('Error loading data:', error);
+    }
+  }
+
+  initRoutes() {
+    this.app.get('/csv', (req, res) => {
+      res.json(this.csvData);
+    });
+
+    this.app.get('/json', (req, res) => {
+      res.json(this.jsonData);
+    });
+
+    this.app.get('/txt', (req, res) => {
+      res.send(this.txtData);
+    });
+
+    this.app.get('/xml', (req, res) => {
+      res.json(this.xmlData);
+    });
+
+    this.app.get('/yaml', (req, res) => {
+      res.json(this.yamlData);
+    });
+  }
+
+  startServer(port = 3000) {
+    this.app.listen(port, () => {
+      console.log(`Data API server running on port ${port}`);
+    });
+  }
+}
+
+// Usage
+async function startAPI() {
+  const dataAPI = new DataAPI();
+  await dataAPI.loadData();
+  dataAPI.startServer();
+}
+
+startAPI();
---
+++ 02_Data_Files/reader_parser.js
@@ -0,0 +1,78 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+const xml2js = require('xml2js');
+const yaml = require('js-yaml');
+const { promisify } = require('util');
+
+const readFileAsync = promisify(fs.readFile);
+
+export class FileParser {
+  static async parseCSV(filePath) {
+    return new Promise((resolve, reject) => {
+      let results = [];
+      fs.createReadStream(filePath)
+        .pipe(csv())
+        .on('data', (row) => results.push(row))
+        .on('end', () => {
+          resolve(results);
+        })
+        .on('error', reject);
+    });
+  }
+
+  static async parseJSON(filePath) {
+    try {
+      const data = await readFileAsync(filePath);
+      return JSON.parse(data);
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  static async parseTXT(filePath) {
+    try {
+      return await readFileAsync(filePath, 'utf8');
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  static async parseXML(filePath) {
+    try {
+      const data = await readFileAsync(filePath);
+      return promisify(xml2js.parseString)(data);
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  static parseYAML(filePath) {
+    try {
+      return yaml.load(fs.readFileSync(filePath, 'utf8'));
+    } catch (e) {
+      throw e;
+    }
+  }
+}
+
+async function parseFiles() {
+  try {
+    const csvData = await FileParser.parseCSV('C:/Users/omerm/SI/02_Data_Files/me.csv');
+    console.log(csvData);
+
+    const jsonData = await FileParser.parseJSON('C:/Users/omerm/SI/02_Data_Files/me.json');
+    console.log(jsonData);
+
+    const txtData = await FileParser.parseTXT('C:/Users/omerm/SI/02_Data_Files/me.txt');
+    console.log(txtData);
+
+    const xmlData = await FileParser.parseXML('C:/Users/omerm/SI/02_Data_Files/me.xml');
+    console.log(xmlData);
+
+    const yamlData = await FileParser.parseYAML('C:/Users/omerm/SI/02_Data_Files/me.yaml');
+    console.log(yamlData);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
